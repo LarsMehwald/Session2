85cd1a2b6370b0f021d43e013446c1504804b78f
LarsMehwald <lars.mehwald@posteo.de>
2015-09-18 13:55:16 +0200

    Summary of R Commands

---
+++ SummaryOfCommandsR.R
@@ -0,0 +1,26 @@
+###########################################
+# Summary of R Commands
+# Lars Mehwald
+###########################################
+
+# open information page for a specific command
+help(CommandName)
+?CommandName # both function the same way
+
+# find datasets in R
+data()
+
+# load dataset from inside of R
+data(NameOfDataset)
+
+# delete dataset
+rm(NameOfDataset)
+
+############################################
+
+# creating a histogram from a variable
+hist(VarName)
+
+# get the mean of a variable
+mean(VarName)
+
